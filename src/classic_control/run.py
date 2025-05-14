19c48a2490aa32a1c349a6e21d62cb64ab43faa7
cneumann01 <carterneumann@protonmail.com>
2025-05-14 10:40:43 -0400

    cart_pole v1

--- src/classic_control/run.py
+++ src/classic_control/run.py
@@ -1,11 +1,14 @@
 import gymnasium as gym
 
-def main():
+def run():
     env = gym.make("CartPole-v1", render_mode="human")
     observation, info = env.reset(seed=42)
 
     for _ in range(1000):
-        action = env.action_space.sample()  # Random action
+        # action = env.action_space.sample()  # Random action
+
+        action = 1 if observation[2] > 0 else 0  # Simple policy: move right if pole is tilted right, else left
+
         observation, reward, terminated, truncated, info = env.step(action)
 
         if terminated or truncated:
@@ -14,4 +17,4 @@ def main():
     env.close()
 
 if __name__ == "__main__":
-    main()
+    run()
